885e8a4268967a7d17cacb661b1a340a9e6e7b43
MerZakhar <merunovych@gmail.com>
2026-04-28 14:57:52 +0300

    Fixed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,14 +5,18 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD = 10;
+    private static final String UKRAINIAN = "Ukrainian";
+
     @Override
     public boolean test(Candidate c) {
         String[] parts = c.getPeriodsInUkr().split("-");
         int start = Integer.parseInt(parts[0]);
         int end = Integer.parseInt(parts[1]);
-        return c.getAge() >= 35
+        return c.getAge() >= MIN_AGE
                 && c.isAllowedToVote()
-                && Objects.equals(c.getNationality(), "Ukrainian")
-                && end - start >= 10;
+                && Objects.equals(c.getNationality(), UKRAINIAN)
+                && end - start >= MIN_PERIOD;
     }
 }
